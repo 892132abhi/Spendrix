f511e6b0b1b0106105454a66fb36146db26fad09
Abhiram S <sabhiram41@mail.com>
2026-06-25 11:47:55 +0530

    CHANGING THE RAG

--- AI_service/app/services/rag_system/embedding.py
+++ AI_service/app/services/rag_system/embedding.py
@@ -1,21 +1,20 @@
 import os
-import google.generativeai as genai
+from google import genai
 from dotenv import load_dotenv
 
 load_dotenv()
 
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-if not GEMINI_API_KEY:
-    raise ValueError("GEMINI_API_KEY is missing")
+client = genai.Client(
+    api_key=os.getenv("GEMINI_API_KEY")
+)
 
-genai.configure(api_key=GEMINI_API_KEY)
-EMBEDDING_MODEL = "models/gemini-embedding-001"
+EMBEDDING_MODEL = "gemini-embedding-001"
 
-def embed(text: str, task_type: str = "RETRIEVAL_DOCUMENT"):
-    """Generates an vector coordinate array mapping text elements."""
-    response = genai.embed_content(
+
+def embed(text: str):
+    response = client.models.embed_content(
         model=EMBEDDING_MODEL,
-        content=text,
-        task_type=task_type,
+        contents=text,
     )
-    return response["embedding"]
+
+    return response.embeddings[0].values
