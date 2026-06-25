c0281eab7552077d8cff0f0b1d6a83fbe90324d9
Abhiram S <sabhiram41@mail.com>
2026-06-25 12:32:24 +0530

    changed the firebase.jskeys

--- Frontend/src/firebase.js
+++ Frontend/src/firebase.js
@@ -4,14 +4,14 @@ import { getMessaging, getToken, onMessage } from "firebase/messaging";
 import api from "./api/instance";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAxOEgpDadidoPy5kCbi39fe3sUDiG4aSk",
-  authDomain: "spendrix-4f6e7.firebaseapp.com",
-  projectId: "spendrix-4f6e7",
-  storageBucket: "spendrix-4f6e7.firebasestorage.app",
-  messagingSenderId: "858515138739",
-  appId: "1:858515138739:web:ae21b786f4998d917e28c6",
-  measurementId: "G-TTE68FD3YX"
-};
+  apiKey: import.meta.env.apiKey,
+  authDomain: import.meta.env.authDomain,
+  projectId: import.meta.env.projectId,
+  storageBucket: import.meta.env.storageBucket,
+  messagingSenderId: import.meta.env.messagingSenderId,
+  appId: import.meta.envappId,
+  measurementId: import.meta.env.measurementId,
+}
 
 const app = initializeApp(firebaseConfig);
 const messaging = getMessaging(app);
@@ -26,8 +26,7 @@ export const requestForToken = async () => {
     }
 
     const token = await getToken(messaging, {
-      vapidKey:
-        "BPPnGd_mPw8dQTT3VxDu15pGJgnVXamfBmGPHJZ8X6ipwpn4WJ3_k_aYGf1d6paBRtDli7Llk6sVBPOy3CpTJKE",
+      vapidKey:import.meta.env.vapidKey,
     });
 
     if (!token) {
